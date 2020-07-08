6efdaffacf6da59c80425edd5d29525057daa965
Jacky Cao <probablyameow+github@gmail.com>
2020-07-08 20:41:02 +0300

    base for new project
    - updated to Swift 5
    - adapted code so outputs at 4K image and fits the iPhone 11 Pro Max resolution
    - I'm intending to create a Core ML version of YOLOv4 and YOLOv4-tiny in future testing

---
+++ README.md
@@ -0,0 +1,9 @@
+# YOLO with Core ML
+
+This repo was forked and modified from [Ma-Dan/YOLOv3-CoreML](https://github.com/Ma-Dan/YOLOv3-CoreML). 
+
+Key things:
+1. I am testing with an iPhone 11 Pro Max, i.e., screen resolution and output is adapted for this phone
+2. My plan is to update the model to use YOLOv4 or YOLOv4-tiny - my hope is to increase the FPS further from ~20/30 FPS to even higher
+
+![YOLO in action](example.png)
---
+++ YOLOv3-CoreML/yolo_coreml/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    
+    // Disable timer for screen turning off
+    UIApplication.shared.isIdleTimerDisabled = true
+    return true
+  }
+    
+}
